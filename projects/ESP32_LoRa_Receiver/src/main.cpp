af11040a8f4b405ce5e7274741a0f87163e1e82a
pieter <Snack101!>
2020-10-04 20:09:01 +0200

    second commit

--- projects/ESP32_LoRa_Receiver/src/main.cpp
+++ projects/ESP32_LoRa_Receiver/src/main.cpp
@@ -1,15 +1,18 @@
+// This script initialises an Arduino framework and uses the Ardiuno LoRa driver. 
+// The Heltec library didn't work for us.
+
 #include <Arduino.h>
 #include <SPI.h>
 #include <LoRa.h>
 
 void setup() {
-  Serial.begin(9600);
-  LoRa.setPins(18, 14, 26);
+  Serial.begin(9600); // Make sure that the baudrate of your setup aligns with this rate!
+  LoRa.setPins(18, 14, 26); // Initializing LoRa device, these settings should fit a Heltec ESP32 LoRa v2 board
   while (!Serial);
 
   Serial.println("LoRa Receiver");
 
-  if (!LoRa.begin(868E6)) {
+  if (!LoRa.begin(868E6)) { // LoRa is set to 868 MHz as this project was run in Europe, please note that this setting is non-trivial, as the board too, have  a limited range.
     Serial.println("Starting LoRa failed!");
     while (1);
   }
